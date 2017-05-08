16673a74b614a6fed8d9ea1b56eb75476aaa7465
王建中 <iamnew@zdongpin.com>
2017-05-08 22:36:21 +0800

    'route'

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import {IndexRoute, Route} from 'react-router';
 import {isLogin} from './redux/modules/login';
-import {Login, App, User, AccountManage} from './containers';
+import {Login, App, User, AccountManage, Privilege, GeneralGoods, DailyTweets} from './containers';
 
 export default (store) => {
     const requireLogin = (nextState, replace, cb) => {
-
         const { login: { user }} = store.getState();
         if (!user) {
             //没有登录留在登录业
@@ -18,8 +17,11 @@ export default (store) => {
     return (
         <Route path="/" component={Login}>
             <Route path="/home" component={App} onEnter={requireLogin}>
-                <Route path="/home/user" component={User}/>
+                <Route path="/home/user" component={User} />
                 <Route path="/home/management" component={AccountManage} />
+                <Route path="/home/privilege" component={Privilege} />
+                <Route path="/home/generalGoods" component={GeneralGoods} />
+                <Route path="/home/dailyTweets" component={DailyTweets} />
             </Route>
         </Route>
     );
